05c6cd816d9561c61f826f3ba21d043868a6248b
Zeynab Naderi <Z.Naderi@Solico-group.com>
2020-09-05 15:52:51 +0430

    student file renamed to car

---
+++ Car.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Z.Naderi
+ * Date: 9/5/2020
+ * Time: 3:15 PM
+ */
+
+class MsnCar
+{
+    public $name;
+    private $wheel_count=4;
+    private $door_count;
+    protected $company;
+    public static $car_static_property="This is for parent class";
+
+    public function __construct($name=null,$wheel_count=null)
+    {
+        $this->name=$name;
+        $this->wheel_count=$wheel_count;
+    }
+
+    public function getCompany()
+    {
+        return $this->company;
+    }
+    public function setCompany($company)
+    {
+        $this->company=$company;
+    }
+    public function getWheelCount()
+    {
+        return $this->wheel_count;
+
+    }
+
+    public function setWheelCount($wheel_count)
+    {
+        $this->wheel_count=$wheel_count;
+
+    }
+}
